6e328df391c3a0011b1d939e6be90c2178913b8f
Braden Richardson <braden.richardson13@gmail.com>
2021-08-01 22:01:15 +0800

    1st challenge

---
+++ challenges.py
@@ -0,0 +1,14 @@
+## Challenges
+
+# Jordan
+# 1. Build a simple calculator
+# - Print the sum of two user inputted values
+def calculate():
+    # Write your code here
+    pass
+
+
+# Braden
+
+
+calculate()

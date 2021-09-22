00f79f3fd4fa4ed0c7ef686fdac718c416a29d9d
ethanpocock <36301346+ethanpocock@users.noreply.github.com>
2021-09-22 21:20:49 +0800

    Challenge #1 Complete
    Built a simple calculator

--- challenges.py
+++ challenges.py
@@ -1,5 +1,23 @@
 ## Challenges
 
+# Ethan
+# 1. Build a simple calculator
+# - Print the sum of two user inputted values
+def calculateSum(totalAddends):
+    print("Calculatron 4000")
+    print("Which numbers would you like to add together?")
+    addends = list(range(totalAddends))
+    sum = 0
+    for n in addends:
+        count = n + 1
+        print("Number {}: ".format(count),end="")
+        addends[n] = input()
+        addends[n] = int(addends[n])
+        sum += addends[n]
+    addends_str = ' + '.join(map(str,addends))
+    print("{} = {}".format(addends_str,sum))
+    input()
+
 # Jordan
 # 1. Build a simple calculator
 # - Print the sum of two user inputted values
@@ -11,4 +29,4 @@ def calculate():
 # Braden
 
 
-calculate()
+calculateSum(2)

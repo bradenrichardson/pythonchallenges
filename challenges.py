02175cc1b61dd3e49c8b8127d6285eace6a470a3
ethanpocock <36301346+ethanpocock@users.noreply.github.com>
2021-09-22 21:45:04 +0800

    Added Challenge #1 for Braden

--- challenges.py
+++ challenges.py
@@ -26,6 +26,9 @@ def calculate():
 
 
 # Braden
+# 1. Create a program which allows a user to input any cryptocurrency ticker 
+# and then receive an output of TRUE or FALSE as to whether that cryptocurrency
+# is below the 50 Day EMA (Exponential Moving Average)
 
 
 calculateSum(2)
